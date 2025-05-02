3a933e56df3a5594275f3bf7a502ba83bc44685c
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2025-05-02 13:08:47 +0200

    add: onyxia init script

--- init_onyxia.sh
+++ init_onyxia.sh
@@ -1,10 +1,20 @@
-pip install transformers
-pip install tf-keras
-pip install --upgrade jupyter
-pip install --upgrade ipywidgets
-pip install 'accelerate>=0.26.0'
-pip install datasets
-pip install peft
-pip install trl
-python3 -c "from huggingface_hub import login; login()"
+#!/bin/bash
+
+# This init script clones a Git repository that contains a Jupyter notebook
+# named `tutorial.ipynb` and opens it in Jupyter Lab at startup
+# Expected parameters : None
+
+# Clone repository and give permissions to the onyxia user
+GIT_REPO=mariusgarenaux/fine_tuning_acronym
+git clone --depth 1 https://github.com/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
+
+# Install additional packages if a requirements.txt file is present in the project
+REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
+# Open the relevant notebook when starting Jupyter Lab
+# echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/inputs/tutorial.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+
+# python3 -c "from huggingface_hub import login; login()"
 # paste token and say n
