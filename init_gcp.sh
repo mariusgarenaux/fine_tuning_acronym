1de275ecd8430cf5c27d053ad0094ad58b2405de
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2025-09-02 16:17:40 +0200

    add venv and proper git branch

--- init_gcp.sh
+++ init_gcp.sh
@@ -1,12 +1,9 @@
-
-
 cd ~
 git clone https://github.com/mariusgarenaux/fine_tuning_acronym
-
-# Creates a bucket for data, models and test results
-# bucket path 
-
 cd fine_tuning_acronym
+
+git checkout formation-continue
+
 BUCKET_PATH="../bucket/fine_tuning_acronym"
 
 # fill bucket with empty folders for test, models and data
@@ -17,5 +14,6 @@ mkdir -p -v ${BUCKET_PATH}/sessions
 cp -i ./example_data/acronym.json ${BUCKET_PATH}/data/acronym.json
 
 
-/opt/conda/bin/pip install -r 00-set_up/requirements.txt
-/opt/conda/bin/python3 -c "from huggingface_hub import login; login()"
+/opt/conda/bin/python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
