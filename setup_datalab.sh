45d6a0473845a86883fcf878c5530a782a993dfc
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2026-06-04 11:21:59 +0200

    removed fuser line

--- setup_datalab.sh
+++ setup_datalab.sh
@@ -28,7 +28,7 @@ sleep 5
 ollama pull qwen3:1.7b
 
 # Stop existing jupyter lab on port 8888
-fuser -k 8888/tcp || true
+# fuser -k 8888/tcp || true
 
 # runs the jupyter lab server
 /root/fine_tuning_acronym/.venv/bin/jupyter-lab --allow-root --IdentityProvider.token=token --ServerApp.allow_remote_access=True --ServerApp.base_url=/notebook --NotebookApp.token=token
