fab2552ff0a74e83faec302ce5bc24dd410b25b1
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2026-06-05 11:30:53 +0200

    added env variable

--- setup_datalab.sh
+++ setup_datalab.sh
@@ -25,10 +25,11 @@ ollama start &
 sleep 5
 
 # pull model
-ollama pull qwen3:1.7b
+ollama pull qwen3:4b
 
 # Stop existing jupyter lab on port 8888
-# fuser -k 8888/tcp || true
+export PYDANTIC_AI_KERNEL_LOG_DIR="/root/.jupyter/logs"
+export PYDANTIC_AI_KERNEL_LOG_LEVEL="DEBUG"
 
 # runs the jupyter lab server
 /root/fine_tuning_acronym/.venv/bin/jupyter-lab --allow-root --IdentityProvider.token=token --ServerApp.allow_remote_access=True --ServerApp.base_url=/notebook --NotebookApp.token=token
